41bc8f2ec2e56cd4f8b3915fa2e1c4c074774029
patgrajkowski <patgrajkowski@gmail.com>
2022-03-05 10:46:58 +0100

    added localhost to cors origin for development purpose

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://helply.vercel.app',
+    origin: ['https://helply.vercel.app', 'http://localhost:3000'],
     credentials: true,
   })
 );
